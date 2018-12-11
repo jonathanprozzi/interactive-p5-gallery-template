fac7a1e01d6656c6ae6128c659f30d7134997fbe
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-10 22:21:24 -0500

    updated data.js and working through Sketch component logic

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -1,6 +1,14 @@
 import { sketchData } from "./data.js";
-
+import p5 from "p5";
 import React, { Component } from "react";
+
+const processSketch = sketchInfo => {
+  console.log(sketchInfo);
+  const runSketch = function(sketchInfo) {
+    `
+    `;
+  };
+};
 
 class Sketch extends Component {
   render() {
@@ -8,6 +16,7 @@ class Sketch extends Component {
       <div>
         <h2>{sketchData[0].title}</h2>
         <main dangerouslySetInnerHTML={{ __html: sketchData[0].content }} />
+        {processSketch(sketchData[0].sketch)}
       </div>
     );
   }
--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,17 @@
 export const sketchData = [
   {
     title: "Test Sketch",
-    content: "<h3>TEST HTML RENDER</h3>"
+    content: `<h2>Canvas Element: My Sketch 1</h2>`,
+    sketch: `
+    function setup() {
+      createCanvas(200,200);
+    }
+    
+    function draw() {
+      background(0);
+      fill(255,0,100);
+      rect(200,200,50,50);
+    }
+    `
   }
 ];
