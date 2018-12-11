9e2a9742d1630dafe5feaec241abe90b2d5ce25d
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-10 23:28:29 -0500

    it works! need to tweak this though as its likely not very hearty

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -2,10 +2,13 @@ import { sketchData } from "./data.js";
 import p5 from "p5";
 import React, { Component } from "react";
 
-let processSketch = (strings, ...values) => {
-  return `${values}`;
+let processSketch = (...values) => {
+  return `${values}
+`;
 };
 
+{
+  /*
 let sketch = new p5(p => {
   const x = 200;
   const y = 200;
@@ -20,12 +23,22 @@ let sketch = new p5(p => {
     p.rect(x / 2, y / 2, 20, 20);
   };
 });
+*/
+}
 
 class Sketch extends Component {
+  componentDidMount() {
+    const sketchScriptElement = document.createElement("script");
+    const sketchScript = document.createTextNode(
+      processSketch(`${sketchData[0].sketch}`)
+    );
+    sketchScriptElement.appendChild(sketchScript);
+    document.body.appendChild(sketchScriptElement);
+  }
   render() {
     return (
       <div>
-        <h2>{sketchData[0].title}</h2>
+        <h2>{sketchData[1].title}</h2>
         <div id="p5sketch" />
       </div>
     );
