7c330e6e2be6dff00cdb8e2e8c8ede4508ee9978
jonathanprozzi <jonathanprozzi@gmail.com>
2018-12-10 22:49:22 -0500

    p5 working instance in instance mode in Sketch

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -2,21 +2,37 @@ import { sketchData } from "./data.js";
 import p5 from "p5";
 import React, { Component } from "react";
 
-const processSketch = sketchInfo => {
-  console.log(sketchInfo);
-  const runSketch = function(sketchInfo) {
-    `
-    `;
-  };
+let processSketch = (strings, ...values) => {
+  return `${values}`;
 };
 
+let sketch = new p5(p => {
+  const x = 100;
+  const y = 100;
+
+  p.setup = function() {
+    p.createCanvas(x, y);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255, 0, 100);
+    p.rect(x / 2, y / 2, 20, 20);
+  };
+});
+
+const sketchOutput = processSketch`${[sketchData[0].sketch]}`;
+
 class Sketch extends Component {
+  componentDidMount() {
+    processSketch`${[sketchData[0].sketch]}`;
+    console.log(sketchOutput);
+  }
   render() {
     return (
       <div>
         <h2>{sketchData[0].title}</h2>
         <main dangerouslySetInnerHTML={{ __html: sketchData[0].content }} />
-        {processSketch(sketchData[0].sketch)}
       </div>
     );
   }
